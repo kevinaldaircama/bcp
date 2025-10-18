afa88cd4c3e9e7ad5a53d4721768d4020b145271
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2025-10-18 11:38:44 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'yape peru_v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/perfil.html',
+  '/imagen/app_icon_xxxhdpi.png',
+  '/imagen/splash_icon_xxxhdpi.png'
+  '/inicio.html'
+  '/login.html'
+];  
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
