ea6a6ce3c43e535fa72b17b904f6f087ee0077ce
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2025-11-01 20:13:25 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,40 @@
-const CACHE_NAME = 'banca móvil BCP_v3';
+// Nombre del caché con versión
+const CACHE_NAME = 'banca-movil-bcp-v3';
+
+// Archivos a cachear
 const urlsToCache = [
   '/',
   '/index.html',
   '/perfil.html',
-  '/imagen/app_icon_xxxhdpi.png',
-  '/imagen/splash_icon_xxxhdpi.png'
-  '/inicio.html'
-  '/login.html'
-];  
+  '/inicio.html',
+  '/login.html',
+  '/images/app_icon_xxxhdpi.png',
+  '/images/splash_icon_xxxhdpi.png'
+];
 
+// Instalar y cachear los recursos
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()) // fuerza instalación inmediata
   );
 });
 
+// Activar y limpiar versiones anteriores
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
+// Interceptar peticiones
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(resp => {
